ea4e984e0055c04cd24f166c9c9c81b8630d6959
harjas <harjas@inceptioncloud.io>
2021-05-03 00:16:14 +0530

    account and contract implementation

--- chain/harmony/contract.go
+++ chain/harmony/contract.go
@@ -2,18 +2,34 @@ package harmony
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/harmony-one/harmony/rpc"
 	"github.com/renproject/multichain/api/address"
 	"github.com/renproject/multichain/api/contract"
 	"github.com/renproject/pack"
 )
 
-func (c Client) CallContract(ctx context.Context, addr address.Address, callData contract.CallData) (pack.Bytes, error) {
+type Params struct {
+	CallArgs rpc.CallArgs
+	Block    uint64
+}
+
+func (c *Client) CallContract(ctx context.Context, addr address.Address, callData contract.CallData) (pack.Bytes, error) {
 	const method = "hmyv2_call"
-	data := []byte(fmt.Sprintf("[{\"to\": \"%s\", \"data\": \"%s\"}]", string(addr), string(callData)))
+	// Unmarshal required to get the block number parameter for the call
+	var callParams Params
+	err := json.Unmarshal(callData, &callParams)
+	if err != nil {
+		return nil, err
+	}
+	args, err := json.Marshal(callParams.CallArgs)
+	if err != nil {
+		return nil, err
+	}
+	data := []byte(fmt.Sprintf("[%s, %d]", string(args), callParams.Block))
 	response, err := SendData(method, data, c.opts.Host)
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 	return pack.NewBytes(*response.Result), nil
